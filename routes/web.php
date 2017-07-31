16f5b548e1d17801badccbe0f00f106df54e908b
Javierson <warrior_alfa92@hotmail.com>
2017-07-30 22:02:00 -0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,128 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Ruta de la pagina de aterrizaje
+
+
+
+Route::get('/', 'HomeController@landingPage');
+
+Route::get('/home', 'HomeController@landingPage');
+
+// Rutas del Sistema
+
+// Gestion
+
+Route::get('/gestion', 'empresaController@gestion');
+
+// Empresa
+
+Route::get('/registrarEmpresa', 'empresaController@registrar');
+
+Route::get('/editarEmpresa/{id}', 'empresaController@editar');
+
+Route::post('/actualizarEmpresa/{id}', 'empresaController@actualizar');
+
+Route::post('/guardarEmpresa', 'empresaController@guardar');
+
+Route::get('/consultarEmpresas', 'empresaController@consultar');
+
+Route::get('/eliminarEmpresa/{id}', 'empresaController@eliminar');
+
+// Sucursal
+
+Route::get('/registrarSucursal', 'sucursalController@registrar');
+
+Route::post('/guardarSucursal', 'sucursalController@guardar');
+
+Route::get('/editarSucursal/{id}', 'sucursalController@editar');
+
+Route::post('/actualizarSucursal/{id}', 'sucursalController@actualizar');
+
+Route::get('/consultarSucursales', 'sucursalController@consultar');
+
+Route::get('/eliminarSucursal/{id}', 'sucursalController@eliminar');
+
+// Vehiculos
+
+Route::get('/registrarVehiculo', 'vehiculoController@registrar');
+
+Route::post('/guardarVehiculo', 'vehiculoController@guardar');
+
+Route::get('/editarVehiculo/{id}', 'vehiculoController@editar');
+
+Route::post('/actualizarVehiculo/{id}', 'vehiculoController@actualizar');
+
+Route::get('/consultarVehiculos', 'vehiculoController@consultar');
+
+Route::get('/eliminarVehiculo/{id}', 'vehiculoController@eliminar');
+
+// Paquetes
+
+Route::get('/registrarPaquete', 'paqueteController@registrar');
+
+Route::post('/guardarPaquete', 'paqueteController@guardar');
+
+Route::get('/editarPaquete/{id}', 'paqueteController@editar');
+
+Route::post('/actualizarPaquete/{id}', 'paqueteController@actualizar');
+
+Route::get('/consultarPaquetes', 'paqueteController@consultar');
+
+Route::get('/eliminarPaquete/{id}', 'paqueteController@eliminar');
+
+// Promocion
+
+Route::get('/registrarPromocion', 'promocionController@registrar');
+
+Route::post('/guardarPromocion', 'promocionController@guardar');
+
+Route::get('/editarPromocion/{id}', 'promocionController@editar');
+
+Route::post('/actualizarPromocion/{id}', 'promocionController@actualizar');
+
+Route::get('/consultarPromociones', 'promocionController@consultar');
+
+Route::get('/eliminarPromocion/{id}', 'promocionController@eliminar');
+
+// Rutas del Cliente
+
+Route::get('/registrarCliente', 'clienteController@registrar');
+
+Route::post('/guardarCliente', 'clienteController@guardar');
+
+Route::get('/consultarClientes', 'clienteController@consultar');
+
+Route::get('/eliminarCliente/{id}', 'clienteController@eliminar');
+
+// Cuentas
+
+// Consultar datos del Cliente
+
+//Route::post('/registrarCuenta', 'cuentaController@Customer');
+
+//
+
+Route::get('/registrarCuenta', 'cuentaController@registrar');
+
+Route::post('/guardarCuenta', 'cuentaController@guardar');
+
+Route::get('/consultarCuentas', 'cuentaController@consultar');
+
+Route::get('/eliminarCuenta/{id}', 'cuentaController@eliminar');
+
+// Mi perfil
+
+Route::get('/miPerfil', 'perfilController@consultar');
+
+Route::get('/editarPerfil/{id}', 'perfilController@editar');
+
+Route::post('/actualizarPerfil/{id}', 'perfilController@actualizar');
+
+Route::get('/evaluar/{id}', 'perfilController@evaluar');
+
+Route::post('/guardarEvaluacion', 'perfilController@guardar');
+
+Auth::routes();
+
+//Route::get('/home', 'HomeController@index')->name('home');
